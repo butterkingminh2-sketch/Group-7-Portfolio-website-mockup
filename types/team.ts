8df231d3d732a77e43c51bbea90f3e6c5057bd59
butterkingminh2-sketch <butterkingminh2@gmail.com>
2026-06-06 08:40:07 +0700

    feat: add TypeScript interfaces for TeamMember, Project, Education

---
+++ types/team.ts
@@ -0,0 +1,35 @@
+export interface Education {
+  degree: string;
+  major: string;
+  university: string;
+  year: number;
+}
+
+export interface Project {
+  title: string;
+  role: string;
+  year: number;
+  description: string;
+  tags: string[];
+  link: string;
+  card_gradient_angle: number;
+}
+
+export interface TeamMember {
+  slug: string;
+  name: string;
+  display_name_bg: string;
+  role: string;
+  avatar: string;
+  avatar_alt: string;
+  linkedin: string;
+  github: string;
+  skills: string[];
+  tools: string[];
+  languages: string[];
+  education: Education;
+  bio_short: string;
+  projects: Project[];
+  order: number;
+  body: string;
+}
